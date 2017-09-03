b17b7efd27b4aa661b6faeaec8ed449e5960c9f1
S H <light.rain@hotmail.com>
2017-09-03 17:05:04 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,14 @@
+
+function forLoop() {
+
+  for (let i = 0, array < 25; i++);
+
+  while ([i = 1]) {
+    [console.log("I am 1 strange loop.")]
+  }
+
+  while )[i > 1] {
+    [console.log("I am ${i} strange loops.")]
+
+  }
+}
